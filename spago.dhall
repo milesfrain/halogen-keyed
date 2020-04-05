26c8f4c7d60a3ccdef0802194c260ab408c9f20b
Miles Frain <miles.frain@colorado.edu>
2020-04-05 16:30:02 -0600

    table, not keyed

--- spago.dhall
+++ spago.dhall
@@ -3,7 +3,8 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "my-project"
-, dependencies = [ "console", "effect", "halogen", "psci-support" ]
+, dependencies =
+  [ "console", "css", "effect", "halogen", "halogen-css", "psci-support" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Example.purs
+++ src/Example.purs
@@ -1,34 +1,60 @@
 module Example where
 
 import Prelude
+import CSS (backgroundColor, grey)
+import Data.Array ((..))
 import Data.Maybe (Maybe(..))
 import Halogen as H
+import Halogen.HTML.CSS as CSS
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 
+data HoverInfo
+  = HoverRow Int
+  | NoHover
+
 type State
-  = { count :: Int }
+  = { hover :: HoverInfo }
 
 data Action
-  = Increment
+  = SetHover HoverInfo
 
 component :: forall q i o m. H.Component HH.HTML q i o m
 component =
   H.mkComponent
-    { initialState: const { count: 0 }
+    { initialState: const { hover: NoHover }
     , render
     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
 
 render :: forall m. State -> H.ComponentHTML Action () m
 render state =
-  HH.div_
-    [ HH.p_ [ HH.text $ "You clicked " <> show state.count <> " times" ]
-    , HH.button
-        [ HE.onClick \_ -> Just Increment ]
-        [ HH.text "Click me" ]
-    ]
+  let
+    mkRow idx =
+      let
+        active = case state.hover of
+          HoverRow i
+            | i == idx -> true
+          _ -> false
+
+        highlight
+          | active = [ CSS.style $ backgroundColor grey ]
+          | otherwise = []
+      in
+        HH.tr
+          ( [ HE.onMouseEnter \_ -> Just (SetHover (HoverRow idx))
+            ]
+              <> highlight
+          )
+          [ HH.td_ [ HH.text $ show idx ] ]
+  in
+    HH.table
+      [ HE.onMouseLeave \_ -> Just (SetHover NoHover)
+      ]
+      $ [ HH.tr_ [ HH.th_ [ HH.text "id" ] ]
+        ]
+      <> map mkRow (1 .. 1000)
 
 handleAction ∷ forall o m. Action → H.HalogenM State Action () o m Unit
 handleAction = case _ of
-  Increment -> H.modify_ \st -> st { count = st.count + 1 }
+  SetHover h -> H.modify_ \st -> st { hover = h }
