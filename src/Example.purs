3d3bd66ba7241cb8bb36a0c755e4435dd6988fa1
Miles Frain <miles.frain@colorado.edu>
2020-04-06 09:32:20 -0600

    missing tbody tag
    Browsers will add a missing tbody tag to tables generated from plain
    htlm, but not if generated from PS/halogen. See reference
    html-only/index.html.
    This tag is required for bootstrap css highlighting to work.

--- src/Example.purs
+++ src/Example.purs
@@ -9,64 +9,33 @@ import Halogen.HTML.Events as HE
 import Halogen.Themes.Bootstrap4 as B
 import Halogen.HTML.Properties as HP
 
-data HoverInfo
-  = HoverRow Int
-  | NoHover
-
 type State
-  = { hover :: HoverInfo
-    , entries :: Array Int
+  = { entries :: Array Int
     }
-
-data Action
-  = SetHover HoverInfo
 
 component :: forall q i o m. H.Component HH.HTML q i o m
 component =
   H.mkComponent
-    { initialState: const { hover: NoHover, entries: 1 .. 1000 }
+    { initialState: const { entries: 1 .. 3 }
     , render
-    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    , eval: H.mkEval $ H.defaultEval
     }
 
-render :: forall m. State -> H.ComponentHTML Action () m
+render :: forall a m. State -> H.ComponentHTML a () m
 render state =
   let
     mkRow idx =
-      let
-        active = case state.hover of
-          HoverRow i
-            | i == idx -> true
-          _ -> false
-
-        highlight
-          -- | active = [ HP.classes [ B.tableActive ] ]
-          | otherwise = []
-
-        keyId
-          | active = show idx <> "-active"
-          | otherwise = show idx
-      in
-        HH.tr
-          ( [ HE.onMouseEnter \_ -> Just (SetHover (HoverRow idx))
-            ]
-              <> highlight
-          )
-          $ map
-              (\str -> HH.td_ [ HH.text str ])
-              [ show idx, keyId ]
+      HH.tr_
+        $ map
+            (\str -> HH.td_ [ HH.text str ])
+            [ show idx, show $ idx * 10 ]
   in
     HH.table
       [ HP.classes [ B.table, B.tableSm, B.tableHover ]
-      , HE.onMouseLeave \_ -> Just (SetHover NoHover)
       ]
       $ [ HH.tr_
             $ map
-                (\str -> HH.th [ HP.classes [ B.colSm1 ] ] [ HH.text str ])
-                [ "id", "keyId" ]
+                (\str -> HH.th_ [ HH.text str ])
+                [ "id", "value" ]
         ]
       <> map mkRow state.entries
-
-handleAction ∷ forall o m. Action → H.HalogenM State Action () o m Unit
-handleAction = case _ of
-  SetHover h -> H.modify_ \st -> st { hover = h }
