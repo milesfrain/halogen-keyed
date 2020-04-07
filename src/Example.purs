4b8703f8cdf04d293a72d36bf07934db17b3f86b
Miles Frain <miles.frain@colorado.edu>
2020-04-07 10:37:20 -0600

    keyed
    40ms 1k rows, occasional 3s gc lag
    10ms 10 rows

--- src/Example.purs
+++ src/Example.purs
@@ -3,10 +3,12 @@ module Example where
 import Prelude
 import Data.Array ((..))
 import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
 import Effect.Class (class MonadEffect)
 import Effect.Class.Console (log)
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Elements.Keyed as HK
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.Themes.Bootstrap4 as B
@@ -45,20 +47,21 @@ render state =
       HoverRow i -> show i
       NoHover -> "Not hovering"
   in
-    HH.div [ HP.class_ B.col ]
-      [ HH.div_ [ HH.text hovTxt ]
-      , HH.table
-          [ HP.classes [ B.table, B.tableSm, B.tableHover ] ]
-          [ HH.thead_
-              [ HH.tr_
-                  $ map
-                      (\str -> HH.th [ HP.classes [ B.colSm1 ] ] [ HH.text str ])
-                      [ "id", "value" ]
+    HK.div [ HP.class_ B.col ]
+      [ Tuple hovTxt $ HH.div_ [ HH.text hovTxt ]
+      , Tuple "same"
+          $ HH.table
+              [ HP.classes [ B.table, B.tableSm, B.tableHover ] ]
+              [ HH.thead_
+                  [ HH.tr_
+                      $ map
+                          (\str -> HH.th [ HP.classes [ B.colSm1 ] ] [ HH.text str ])
+                          [ "id", "value" ]
+                  ]
+              , HH.tbody
+                  [ HE.onMouseLeave \_ -> Just (SetHover NoHover) ]
+                  $ map mkRow state.entries
               ]
-          , HH.tbody
-              [ HE.onMouseLeave \_ -> Just (SetHover NoHover) ]
-              $ map mkRow state.entries
-          ]
       ]
 
 handleAction ∷ forall o m. MonadEffect m => Action -> H.HalogenM State Action () o m Unit
